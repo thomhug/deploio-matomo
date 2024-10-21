0c972f2993d16bf852b626729c6d23a1f7ebc787
Thomas Hug <tom@anbeda.ch>
2024-10-21 23:26:54 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,59 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
-# Generate a random salt for Matomo
-MATOMO_SALT=$(openssl rand -hex 32)
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	local varValue=$(env | grep -E "^${var}=" | sed -E -e "s/^${var}=//")
+	local fileVarValue=$(env | grep -E "^${fileVar}=" | sed -E -e "s/^${fileVar}=//")
+	if [ -n "${varValue}" ] && [ -n "${fileVarValue}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	if [ -n "${varValue}" ]; then
+		export "$var"="${varValue}"
+	elif [ -n "${fileVarValue}" ]; then
+		export "$var"="$(cat "${fileVarValue}")"
+	elif [ -n "${def}" ]; then
+		export "$var"="$def"
+	fi
+	unset "$fileVar"
+}
 
-# Create the config.ini.php file using environment variables
-cat << EOF > /var/www/html/config/config.ini.php
-[database]
-host = "${MATOMO_DATABASE_HOST}"
-username = "${MATOMO_DATABASE_USERNAME}"
-password = "${MATOMO_DATABASE_PASSWORD}"
-dbname = "${MATOMO_DATABASE_DBNAME}"
-port = "${MATOMO_DATABASE_PORT}"
-tables_prefix = "matomo_"
+file_env 'MATOMO_DATABASE_HOST'
+file_env 'MATOMO_DATABASE_USERNAME'
+file_env 'MATOMO_DATABASE_PASSWORD'
+file_env 'MATOMO_DATABASE_DBNAME'
 
-[General]
-salt = "${MATOMO_SALT}"
+if [ ! -e matomo.php ]; then
+	uid="$(id -u)"
+	gid="$(id -g)"
+	if [ "$uid" = '0' ]; then
+		case "$1" in
+			apache2*)
+				user="${APACHE_RUN_USER:-www-data}"
+				group="${APACHE_RUN_GROUP:-www-data}"
 
-EOF
+			# strip off any '#' symbol ('#1000' is valid syntax for Apache)
+			user="${user#'#'}"
+			group="${group#'#'}"
+			;;
+			*) # php-fpm
+				user='www-data'
+				group='www-data'
+				;;
+		esac
+	else
+		user="$uid"
+		group="$gid"
+	fi
+	tar cf - --one-file-system -C /usr/src/matomo . | tar xf -
+	chown -R "$user":"$group" .
+fi
 
-# Run Apache in the foreground
-apache2-foreground
+exec "$@"
